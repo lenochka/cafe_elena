353780bf66f8c8cb9b960475d156c1d1c2fa31ac
Elena Lysenko <elena.d.lysenko@gmail.com>
2024-12-31 14:40:01 +0100

    Setup Java Script with Bootstrap 5. Trying to fix JS for bootstrap nav dropdown

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,10 @@
+import "@hotwired/turbo-rails"
+import "popper"
+import "bootstrap"
 import { Application } from "@hotwired/stimulus"
+
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
 
 const application = Application.start()
 
@@ -7,3 +13,12 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+console.log('Dropdown');
+const myDropdown = document.getElementById(
+  'dropdownMenuButton1',
+);
+
+myDropdown.addEventListener('show.bs.dropdown', function () {
+  console.log('Dropdown shown');
+});
