be37e2bb91fc6f1cebaa7afedf11563b89d0fd9e
Elena Lysenko <elena.d.lysenko@gmail.com>
2025-01-12 08:00:19 +0100

    Add user edit form. Add user validations. Fix style user new and edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   has_many :products
+  validates :user_name, presence: true, length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email_address, presence: true,
+            uniqueness: true,
+            length: { maximum: 100 },
+            format: { with: VALID_EMAIL_REGEX }
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
