23fefe4a2288018afc0aedae6802f101ad4df495
Elena Lysenko <elena.d.lysenko@gmail.com>
2025-01-26 18:35:16 +0100

    Add Role migration, model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_07_131147) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_26_112337) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,6 +24,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_07_131147) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade do |t|
